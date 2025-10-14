d0d5411241f54f4810cf60b7a26be581629ab2fc
Tsue_Hic22009 <hic22009@byui.edu>
2025-10-14 07:47:23 -0500

    Flask-Login back with user_loader --- Updated User model for Flask-Login compatibility

--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,9 @@
 from app.extensions import db
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
@@ -13,13 +14,32 @@ class User(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
     def set_password(self, password):
+        """Hash and set the user's password"""
         self.password_hash = generate_password_hash(password)
     
     def check_password(self, password):
+        """Check if provided password matches hash"""
         return check_password_hash(self.password_hash, password)
     
     def is_admin(self):
+        """Check if user has admin role"""
         return self.role == 'admin'
+    
+    # Flask-Login requirements
+    def get_id(self):
+        return str(self.id)
+    
+    @property
+    def is_active(self):
+        return True
+    
+    @property
+    def is_authenticated(self):
+        return True
+    
+    @property
+    def is_anonymous(self):
+        return False
     
     def __repr__(self):
         return f'<User {self.username}>'
