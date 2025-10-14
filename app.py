a0ce784f46129bfffa0e9b6224ffa39a4d99f45e
Tsue_Hic22009 <hic22009@byui.edu>
2025-10-13 22:38:56 -0500

    Week 2 Day 1: Initialize database with SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 import os
 
 # Initialize extensions
 db = SQLAlchemy()
-login_manager = LoginManager()
 
 def create_app(config_name='development'):
     # Tell Flask where to find templates - inside the app folder
@@ -17,8 +15,6 @@ def create_app(config_name='development'):
     app.config.from_object(config[config_name])
     
     db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'auth.login'
     
     # Import and register blueprints
     from app.routes import main, auth, admin
